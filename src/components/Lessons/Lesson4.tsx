0e148da19dbba6aec3dc7ece5096d51327c73919
kingk <kingk@DESKTOP-Q0U2A5C>
2025-03-26 14:32:35 +0700

    Lesson 4

---
+++ src/components/Lessons/Lesson4.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import OnBlurForm from '../OnBlurForm'
+import OnSubmitForm from '../OnSubmitForm'
+
+const Lesson4: React.FC = () => {
+  return (
+    <div>
+      <OnBlurForm />
+      <OnSubmitForm />
+    </div>
+  )
+}
+
+export default Lesson4
