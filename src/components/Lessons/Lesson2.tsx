c45ac2474047073d1ed662656751d0bfa01c465b
kingk <kingk@DESKTOP-Q0U2A5C>
2025-03-26 14:32:14 +0700

    Lesson 2

---
+++ src/components/Lessons/Lesson2.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Lesson2: React.FC = () => {
+  return (
+    <div>
+      <p className='text-xl font-bold pb-5'>
+        Thực hành router
+      </p>
+      <p>
+      Sử dụng react router dom để tổ chức router cho các màn sau: Đăng nhập, đăng ký, quên mật khẩu, welcome page, danh sách bài tập, chi tiết bài tập, layout sau khi đăng nhập ( header, sidebar )
+
+Sử dụng redux để lưu trạngthái đăng nhập
+      </p>
+    </div>
+  )
+}
+
+export default Lesson2
